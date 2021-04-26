f2a84aa1df5ad1da60b9d7bb7d7279c1cc30ea94
RobertKochanski <kochanskirobert1999@gmail.com>
2021-04-26 18:39:27 +0200

    Added edit method

--- AnimalsCrossing/AnimalCrossing.Services/RestModels/Animals/UpdateAnimalRequest.cs
+++ AnimalsCrossing/AnimalCrossing.Services/RestModels/Animals/UpdateAnimalRequest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AnimalCrossing.Services.RestModels.Animals
+﻿namespace AnimalCrossing.Services.RestModels.Animals
 {
     public class UpdateAnimalRequest
     {
